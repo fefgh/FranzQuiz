48b1838eb10c25ba2fec1e5aa43a15a5fea85b33
fefgh <87654697+fefgh@users.noreply.github.com>
2024-06-25 08:23:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,49 +2,49 @@ const quizData = [
     {
         question: "Vous aimez les sports en plein air?",
         answers: {
-            a: { text: "Je déteste", points: { football: 2, basket: 4, volley: 3, badminton: 5, courir: 1 } },
-            b: { text: "Je ne l'aime pas", points: { football: 2, basket: 5, volley: 3, badminton: 4, courir: 1 } },
-            c: { text: "Je suis neutre", points: { football: 3, basket: 3, volley: 5, badminton: 2, courir: 2 } },
-            d: { text: "Je l'aime", points: { football: 5, basket: 3, volley: 4, badminton: 1, courir: 4 } },
-            e: { text: "Je l'adore", points: { football: 4, basket: 2, volley: 3, badminton: 0, courir: 5 } }
+            a: { text: "Je déteste", points: { foot: 2, basket: 4, volley: 3, badminton: 5, courir: 1 } },
+            b: { text: "Je ne l'aime pas", points: { foot: 2, basket: 5, volley: 3, badminton: 4, courir: 1 } },
+            c: { text: "Je suis neutre", points: { foot: 3, basket: 3, volley: 5, badminton: 2, courir: 2 } },
+            d: { text: "Je l'aime", points: { foot: 5, basket: 3, volley: 4, badminton: 1, courir: 4 } },
+            e: { text: "Je l'adore", points: { foot: 4, basket: 2, volley: 3, badminton: 0, courir: 5 } }
         }
     },
     {
         question: "Vous aimez jouer en équipe?",
         answers: {
-            a: { text: "Je déteste", points: { football: 0, basket: 1, volley: 1, badminton: 3, courir: 5 } },
-            b: { text: "Je ne l'aime pas", points: { football: 1, basket: 2, volley: 1, badminton: 2, courir: 4 } },
-            c: { text: "Je suis neutre", points: { football: 2, basket: 2, volley: 3, badminton: 5, courir: 2 } },
-            d: { text: "Je l'aime", points: { football: 4, basket: 4, volley: 5, badminton: 3, courir: 1 } },
-            e: { text: "Je l'adore", points: { football: 5, basket: 2, volley: 4, badminton: 3, courir: 0 } }
+            a: { text: "Je déteste", points: { foot: 0, basket: 1, volley: 1, badminton: 3, courir: 5 } },
+            b: { text: "Je ne l'aime pas", points: { foot: 1, basket: 2, volley: 1, badminton: 2, courir: 4 } },
+            c: { text: "Je suis neutre", points: { foot: 2, basket: 2, volley: 3, badminton: 5, courir: 2 } },
+            d: { text: "Je l'aime", points: { foot: 4, basket: 4, volley: 5, badminton: 3, courir: 1 } },
+            e: { text: "Je l'adore", points: { foot: 5, basket: 2, volley: 4, badminton: 3, courir: 0 } }
         }
     },
     {
         question: "Vous aimez jouer avec des balles?",
         answers: {
-            a: { text: "Je déteste", points: { football: 1, basket: 1, volley: 1, badminton: 1, courir: 5 } },
-            b: { text: "Je ne l'aime pas", points: { football: 2, basket: 2, volley: 2, badminton: 2, courir: 3 } },
-            c: { text: "Je suis neutre", points: { football: 3, basket: 3, volley: 3, badminton: 3, courir: 2 } },
-            d: { text: "Je l'aime", points: { football: 4, basket: 4, volley: 4, badminton: 4, courir: 0 } },
-            e: { text: "Je l'adore", points: { football: 5, basket: 5, volley: 5, badminton: 5, courir: 0 } }
+            a: { text: "Je déteste", points: { foot: 1, basket: 1, volley: 1, badminton: 1, courir: 5 } },
+            b: { text: "Je ne l'aime pas", points: { foot: 2, basket: 2, volley: 2, badminton: 2, courir: 3 } },
+            c: { text: "Je suis neutre", points: { foot: 3, basket: 3, volley: 3, badminton: 3, courir: 2 } },
+            d: { text: "Je l'aime", points: { foot: 4, basket: 4, volley: 4, badminton: 4, courir: 0 } },
+            e: { text: "Je l'adore", points: { foot: 5, basket: 5, volley: 5, badminton: 5, courir: 0 } }
         }
     },
     {
         question: "Vous aimez les sports intenses?",
         answers: {
-            a: { text: "Je déteste", points: { football: 1, basket: 1, volley: 1, badminton: 2, courir: 0 } },
-            b: { text: "Je ne l'aime pas", points: { football: 1, basket: 2, volley: 1, badminton: 2, courir: 1 } },
-            c: { text: "Je suis neutre", points: { football: 3, basket: 4, volley: 4, badminton: 4, courir: 3 } },
-            d: { text: "Je l'aime", points: { football: 5, basket: 2, volley: 3, badminton: 4, courir: 4 } },
-            e: { text: "Je l'adore", points: { football: 4, basket: 3, volley: 3, badminton: 4, courir: 5 } }
+            a: { text: "Je déteste", points: { foot: 1, basket: 1, volley: 1, badminton: 2, courir: 0 } },
+            b: { text: "Je ne l'aime pas", points: { foot: 1, basket: 2, volley: 1, badminton: 2, courir: 1 } },
+            c: { text: "Je suis neutre", points: { foot: 3, basket: 4, volley: 4, badminton: 4, courir: 3 } },
+            d: { text: "Je l'aime", points: { foot: 5, basket: 2, volley: 3, badminton: 4, courir: 4 } },
+            e: { text: "Je l'adore", points: { foot: 4, basket: 3, volley: 3, badminton: 4, courir: 5 } }
         }
     },
     {
         question: "Est-ce que vous êtes sportif?",
         answers: {
-            a: { text: "Non", points: { football: 1, basket: 1, volley: 4, badminton: 2, courir: 3 } },
-            b: { text: "Un peu", points: { football: 3, basket: 2, volley: 3, badminton: 4, courir: 4 } },
-            c: { text: "Oui", points: { football: 5, basket: 5, volley: 5, badminton: 5, courir: 5 } }
+            a: { text: "Non", points: { foot: 1, basket: 1, volley: 4, badminton: 2, courir: 3 } },
+            b: { text: "Un peu", points: { foot: 3, basket: 2, volley: 3, badminton: 4, courir: 4 } },
+            c: { text: "Oui", points: { foot: 5, basket: 5, volley: 5, badminton: 5, courir: 5 } }
         }
     }
 ];
@@ -54,7 +54,7 @@ const resultsContainer = document.getElementById('results');
 const submitButton = document.getElementById('submit');
 
 let currentQuestion = 0;
-let totalPoints = { football: 0, basket: 0, volley: 0, badminton: 0, courir: 0 };
+let totalPoints = { foot: 0, basket: 0, volley: 0, badminton: 0, courir: 0 };
 
 function buildQuiz() {
     const currentQuizData = quizData[currentQuestion];
